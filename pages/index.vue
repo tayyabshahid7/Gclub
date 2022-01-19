582d864bfba69752c5fc5fa7ee37fa45185e731d
arslan0510 <arslanjutt23@gmail.com>
2022-01-19 21:15:13 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <signin /> -->
+  <signup />
+</template>
+
+<script>
+import signin from "./signin.vue";
+import Signup from "./signup.vue";
+
+export default {
+  components: { signin, Signup },
+  name: "IndexPage",
+};
+</script>
