e3d3da585cfc1d007c5b79535334ab9c2a5257a0
Jude Lee <judescripts@gmail.com>
2022-02-04 10:14:11 -0800

    homepage hero add scroll icon

--- routes/landing-page/hero/index.vue
+++ routes/landing-page/hero/index.vue
@@ -5,8 +5,19 @@
       class="object-cover w-full h-full bg-cover bg-center"
       alt="hero-bg"
     />
-    <div class="flex justify-center items-center inset-7 absolute image-border">
-      <img class="shrink w-3/5" src="~/assets/images/logo/Wordmark.svg" />
+    <div
+      class="flex flex-col justify-center items-center inset-7 absolute image-border"
+    >
+      <div class="shrink w-3/5 mt-96">
+        <img
+          class="w-full object-contain"
+          src="~/assets/images/logo/Wordmark.svg"
+        />
+      </div>
+      <div class="hero-text flex flex-col items-center justify-center">
+        <p>Scroll Down</p>
+        <img class="mt-10" width="70" src="~/assets/images/arrow-down.png" alt="down arrow" />
+      </div>
     </div>
   </div>
 </template>
@@ -21,5 +32,17 @@ export default {
 .image-border {
   border: 1px solid rgba(250, 250, 250, 0.6);
   box-sizing: border-box;
+}
+
+.hero-text {
+  margin-top: 350px;
+  font-family: Acumin Variable Concept;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 16px;
+  letter-spacing: 0.12em;
+  text-transform: uppercase;
+  color: #ffffff;
 }
 </style>
