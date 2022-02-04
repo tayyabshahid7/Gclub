cfae6414e6b4db24d1f17ba1b1f19225f6028435
Tayyab <tayyab.shahid7@gmail.com>
2022-02-04 22:24:30 +0500

    privacy policy done

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NavbarWhite />
+    <HeroSection/>
+    <QuestionAnswer/>
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import NavbarWhite from "~~/layouts/white-navbar";
+import AppFooter from "~~/layouts/app-footer.vue";
+import HeroSection from "~~/routes/privacy-policy/HeroSection";
+import QuestionAnswer from "~~/routes/privacy-policy/PrivacyData";
+export default {
+  components: {
+    NavbarWhite,
+    HeroSection,
+    QuestionAnswer,
+    AppFooter,
+  },
+  name: "PrivacyPolicy",
+};
+</script>
+
+
+<style lang="scss" scoped></style>
